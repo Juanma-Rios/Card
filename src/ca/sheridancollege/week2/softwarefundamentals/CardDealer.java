8289ba584fb66a1f7e8a93f69c043aa35cfebbf0
juan1 <riosmore@shernet.sheridancollege.ca>
2022-02-01 13:02:41 -0500

    added card object Student Number: 991641559 Juan Rios

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -2,18 +2,23 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Student Number: 991641559
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * The class where we create Cards
+ *
  * @author dancye, 2019
  */
-public class CardDealer 
+public class CardDealer
 {
-    public static void main(String[] args)
-    {
-        Card nineClubs = new Card("Clbs", 9);
-        nineClubs.setSuit("diamonds");
-    }
+   public static void main (String[] args)
+   {
+      Card nineClubs = new Card("Clbs", 9);
+      nineClubs.setSuit("diamonds");
+
+      Card juanRiosCard = new Card("Spds", 1);
+      juanRiosCard.setSuit("spades");
+   }
 }
